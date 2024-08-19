df69d7f5dc45e795da971c9272074fa2a315ea4b
darrenzhang <darrenzhang@awoo.com.tw>
2024-08-19 21:08:49 +0800

    add case to intercept request

---
+++ intercept.test.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@playwright/test');
+
+test('test intercept request', async ({ page }) => {
+    await page.route('**/api/data', (route, request) => {
+        console.log('Intercepted request:', request.postData());
+        const postData = JSON.parse(request.postData());
+        expect(postData.location).toBe('Asia/Taipei')
+        route.continue();
+    });
+
+    await page.goto('http://localhost:3000');
+    await page.click('#sendRequest');
+
+    // 讓時間留給網頁處理請求
+    await page.waitForTimeout(1000);
+});
