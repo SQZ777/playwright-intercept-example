1686abaf17bc7fb371c6d7bbd431463452adc5c1
darrenzhang <darrenzhang@awoo.com.tw>
2024-08-20 09:54:58 +0800

    remove europe

--- intercept.test.js
+++ intercept.test.js
@@ -16,22 +16,3 @@ test.describe('test with local and no change', () => {
         await page.waitForTimeout(1000);
     });
 });
-
-test.describe('test with french language block', () => {
-    test.use({ timezoneId: 'Europe/Paris'});
-    test('test intercept request for change location', async ({ page, context }) => { 
-        await page.route('**/api/data', (route, request) => {
-            console.log('Intercepted request:', request.postData());
-            const postData = JSON.parse(request.postData());
-            expect(postData.location).toBe('Europe/Paris')
-            route.continue();
-        });
-        await page.goto('http://localhost:3000');
-        await page.click('#sendRequest');
-    
-        // 讓時間留給網頁處理請求
-        await page.waitForTimeout(1000);
-    });
-    
-});
-
