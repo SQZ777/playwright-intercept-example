8d6d9a69882fcaeaea5f003799242c26d66fd6bb
darrenzhang <darrenzhang@awoo.com.tw>
2024-08-20 09:53:21 +0800

    change location

--- intercept.test.js
+++ intercept.test.js
@@ -1,16 +1,37 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect, defineConfig } = require('@playwright/test');
 
-test('test intercept request', async ({ page }) => {
-    await page.route('**/api/data', (route, request) => {
-        console.log('Intercepted request:', request.postData());
-        const postData = JSON.parse(request.postData());
-        expect(postData.location).toBe('Asia/Taipei')
-        route.continue();
+test.describe('test with local and no change', () => {
+    test('test intercept request for no change', async ({ page }) => {
+        await page.route('**/api/data', (route, request) => {
+            console.log('Intercepted request:', request.postData());
+            const postData = JSON.parse(request.postData());
+            expect(postData.location).toBe('Asia/Taipei')
+            route.continue();
+        });
+    
+        await page.goto('http://localhost:3000');
+        await page.click('#sendRequest');
+    
+        // 讓時間留給網頁處理請求
+        await page.waitForTimeout(1000);
     });
-
-    await page.goto('http://localhost:3000');
-    await page.click('#sendRequest');
-
-    // 讓時間留給網頁處理請求
-    await page.waitForTimeout(1000);
 });
+
+test.describe('test with french language block', () => {
+    test.use({ timezoneId: 'Europe/Paris'});
+    test('test intercept request for change location', async ({ page, context }) => { 
+        await page.route('**/api/data', (route, request) => {
+            console.log('Intercepted request:', request.postData());
+            const postData = JSON.parse(request.postData());
+            expect(postData.location).toBe('Europe/Paris')
+            route.continue();
+        });
+        await page.goto('http://localhost:3000');
+        await page.click('#sendRequest');
+    
+        // 讓時間留給網頁處理請求
+        await page.waitForTimeout(1000);
+    });
+    
+});
+
